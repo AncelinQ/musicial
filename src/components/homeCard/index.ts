c42f63bf2765da5844e2e0a979e67d76f04b1875
Ancelin <57001956+AncelinQ@users.noreply.github.com>
2021-11-02 14:05:49 +0100

    folder change to contain client and api folders + added symfony api folder with first entities and controllers

--- src/components/homeCard/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HomeCard } from './homeCard'
-
--- src/layouts/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as StandardLayout } from './standard-layout';
-export { default as FlexLayout } from './flex-layout';
-
-
